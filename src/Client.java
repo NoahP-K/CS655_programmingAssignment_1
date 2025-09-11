d046ce5aee40ecc98a80d75d992ad1d1f8f1fc6b
NoahP-K <noahpk@bu.edu>
2025-09-11 14:05:35 -0400

    It still is not finished, but it compiles for the sake of testing Echo

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
 //Class to hold functions for running the client portions of the assignment
 public class Client {
@@ -43,10 +46,10 @@ public class Client {
         String type;    //rtt or tput
         int msgSize;    //num. bytes in payload
         int probeNum;
-        int[] measurements;     //collection of all measurements for this type+size
+        long[] measurements;     //collection of all measurements for this type+size
 
         public MeasurementResults(int numMeasurements, String type, int msgSize) {
-            measurements = new int[numMeasurements];
+            measurements = new long[numMeasurements];
             probeNum = numMeasurements;
             this.type = type;
             this.msgSize = msgSize;
@@ -77,23 +80,23 @@ public class Client {
 
         //Run RTT measurements
         for(int i=0; i<rttFiles.length; i++) {
-            MeasurementResults results = MeasurementResults(Client.probeNum, "rtt", rttMsgSizes[i]);
+            Client.MeasurementResults results = new MeasurementResults(Client.probeNum, "rtt", rttMsgSizes[i]);
             //CSP
             connectionSetupPhase(connection, results, 0);
             //MP
-            measurementPhase()
+            //measurementPhase()
         }
     }
 
     //perform the connection setup phase
     private static boolean connectionSetupPhase(
             ClientConnection connection,
-            MeasurementResults results,
+            Client.MeasurementResults results,
             int serverDelay
     ) {
         //send CSP message to server
         connection.out.println(
-                "s %s %d %d %d\n".formatted(results.type, results.probeNum, results.msgSize, serverDelay)
+               "s %s %d %d %d\n".formatted(results.type, results.probeNum, results.msgSize, serverDelay)
         );
         while(true) {
             try {
@@ -110,21 +113,38 @@ public class Client {
             ClientConnection connection,
             int serverDelay,
             String file,
-            MeasurementResults results
+            Client.MeasurementResults results
     ) {
         //Send a number of messages containing the file contents
-        String fileContents = new String(Files.readAllBytes(Paths.get(
-                        "../data/%s/%s".format(results.type, file)
-        )), StandardCharsets.UTF_8);
+        String fileContents;
+        try {
+            fileContents = new String(Files.readString(Paths.get(
+                    "../data/%s/%s".formatted(results.type, file)
+            )));
+        } catch (IOException e) {
+            System.err.println("Failed to read file " + file + ". Skipping this measurement phase.");
+            return true;
+        }
         for(int i=0; i<results.probeNum; i++) {
+            long start = System.nanoTime();
             connection.out.println(
-                    "w %d %s\n".format(i, fileContents)
+                    "w %d %s\n".formatted(i, fileContents)
             );
             try {
-                System.out.println("echo: " + connection.in.readLine());
+                String response = connection.in.readLine();
+                long end = System.nanoTime();
+                long timeElapsed = end - start;
+                System.out.println((response.length()<10)
+                                ? "Echo: " + response
+                                : "Echo: " + response.substring(0, 10) + "..."
+                );
+                System.out.println("Time elapsed: " + timeElapsed);
+                if(results.type == "rtt") {results.measurements[i] = timeElapsed;}
+                //else if(results.)
             } catch (IOException e) {
                 System.err.println("Failed to echo.");
             }
         }
+        return true;
     }
 }
